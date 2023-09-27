45fdbe54738bfdeb8d944fbb2dd3682e7246c22e
Olena Zdanovska <freshjs@gmail.com>
2023-09-27 11:36:43 +0300

    Add input range

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addCount, subCount } from './../../store/counterSlice';
+import { addCount, subCount, setStep } from './../../store/counterSlice';
 
 const Counter = (props) => {
-  const {count} = useSelector((state)=>state.counter);
+  const { count, step } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
-  const handleAdd = () => dispatch(addCount({step:7}));
-  const handleSub = () => dispatch(subCount({step:7}));
+  const handleAdd = () => dispatch(addCount({ step }));
+  const handleSub = () => dispatch(subCount({ step }));
+  const handleStep = ({ target: { value } }) =>
+    dispatch(setStep({ step: Number(value) }));
   return (
     <section>
       <h2>Count: {count}</h2>
+      <div>
+        <input type="range" value={step} onChange={handleStep} />
+        <span>{step}</span>
+      </div>
       <button onClick={handleAdd}>+</button>
       <button onClick={handleSub}>-</button>
     </section>
--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -4,22 +4,21 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState: {
     count: 100,
-    prop1:10,
-    prop2:'qwe'
+    step: 1,
   },
   reducers: {
-    addCount(state, action){
-      console.log('state', state);
-      console.log('action', action);
-      // state.count+=action.payload
-      state.count+= action.payload.step
+    addCount(state, action) {
+      state.count += action.payload.step;
     },
-    subCount(state, action){
-      state.count-= action.payload.step
+    subCount(state, action) {
+      state.count -= action.payload.step;
+    },
+    setStep(state, action) {
+      state.step = action.payload.step;
     },
   },
 });
 
-export const {addCount, subCount} = counterSlice.actions;
+export const { addCount, subCount, setStep } = counterSlice.actions;
 
 export default counterSlice.reducer;
