eacfe7b554de3707599b73080d3f9de9b4b97f94
Olena Zdanovska <freshjs@gmail.com>
2023-09-27 09:56:43 +0300

    Initial commit

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Counter = (props) => {
+  return <section>
+    <h2>Count: {0}</h2>
+    <button>+</button>
+    <button>-</button>
+  </section>;
+};
+
+export default Counter;
